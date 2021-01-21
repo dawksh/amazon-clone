0735151d053eae038032a0526d40704bef2753e2
Daksh Kulshreshtha <akshd@outlook.com>
2021-01-21 22:18:38 +0530

    Navbar done

--- src/components/sub-components/Nav.js
+++ src/components/sub-components/Nav.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import amazon from '../../assets/amazon.png'
+import india from '../../assets/india.png'
+import cart from '../../assets/cart.png'
 
 export default function Nav() {
 
@@ -15,7 +17,7 @@ export default function Nav() {
 
     return (
         <nav>
-            <img src={amazon} alt="" />
+            <img src={amazon} alt="amazon logo" />
             <div className="deliver">
                 <span className="light">Deliver to Daksh</span>
                 <br />
@@ -25,7 +27,21 @@ export default function Nav() {
                 <input type="text" className="search" />
                 <button className="searchBtn">🔍</button>
             </div>
-
+            <div>
+                <span className="country"><img src={india} alt="India Flag" /></span>
+            </div>
+            <div className="account">
+                <span className="topText">Hello, Daksh,</span>
+                <span className="bottomText">Account & Lists</span>
+            </div>
+            <div className="account">
+                <span className="topText">Returns</span>
+                <span className="bottomText">& Orders</span>
+            </div>
+            <div className="cart">
+                <img src={cart} alt="cart icon" />
+                <span className="cartText">Cart</span>
+            </div>
         </nav>
     )
 }
